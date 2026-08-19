57c0e8b24ab9ca40d50c41102bbe957ce80f5a2e
sujeong_min <sujeong.min@linecorp.com>
2026-08-19 20:20:04 +0900

    타일링 및 플레이뷰 개선

--- scripts/lobby_scene.gd
+++ scripts/lobby_scene.gd
@@ -1,9 +1,18 @@
 @tool
 extends Node3D
 
+const LOBBY_GROUND_LAND_MATERIAL: StandardMaterial3D = preload("res://resources/lobby_ground_land_material.tres")
+const LOBBY_GROUND_WATER_MATERIAL: StandardMaterial3D = preload("res://resources/lobby_ground_water_material.tres")
+
 @export_group("Lobby Flower Glow")
 @export_range(0.0, 8.0, 0.05) var flower_glow_energy_cap := 1.4
 @export_range(0.1, 4.0, 0.05) var flower_glow_range_cap := 0.65
+
+
+func _ready() -> void:
+	var ground_mesh := get_node("LobbyGround/LobbyGround") as MeshInstance3D
+	ground_mesh.set_surface_override_material(0, LOBBY_GROUND_LAND_MATERIAL)
+	ground_mesh.set_surface_override_material(1, LOBBY_GROUND_WATER_MATERIAL)
 
 
 func _process(_delta: float) -> void:
